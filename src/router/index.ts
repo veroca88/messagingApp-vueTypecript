2b9605f2941eb1fa21cf8c719b528e65c46e81e4
veroca88 <veronicadonoso1506@gmail.com>
2021-02-04 18:14:24 -0600

    Refactor code, delete default pages, add new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,24 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    alias: "/messages",
+    name: "messages",
+    component: () => import("../components/MessagesList.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/messages/:id",
+    name: "message-detail",
+    component: () => import("../components/Message.vue")
+  },
+  {
+    path: "/add",
+    name: "add",
+    component: () => import("../components/AddMessage.vue")
   }
 ];
 
